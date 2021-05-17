db742154035bbf1af010ce5bf194351093bf6b63
taka-inaba <takahiro.inaba@bizmates.jp>
2021-05-17 14:33:15 +0900

    update

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -27,15 +27,19 @@
                                 <a>{{ $user->nationality }}</a>
                             </div>
                             <div class="d-flex align-items-center">
-                                @if (Auth::user()->is_following($user->id) || Auth::user()->id == $user->id)
-                                <div align='right' class="ml-auto">
-                                    <a href="{{ route('user.unfollow', ['unfollowed_id' => $user->id]) }}" class='btn btn-danger'>Unfollow</a>
+                                <div class="d-flex align-items-center">
+                                    @if (Auth::user()->id != $user->id)
+                                        @if (Auth::user()->is_following($user->id) || Auth::user()->id == $user->id)
+                                        <div align='right' class="ml-auto">
+                                            <a href="{{ route('user.unfollow', ['unfollowed_id' => $user->id]) }}" class='btn btn-danger'>Unfollow</a>
+                                        </div>
+                                        @else
+                                        <div align='right' class="ml-auto" >
+                                            <a href="{{ route('user.follow', ['followed_id' => $user->id]) }}" class='btn btn-primary'>Follow</a>
+                                        </div>
+                                        @endif
+                                    @endif
                                 </div>
-                                @else
-                                <div align='right' class="ml-auto" >
-                                    <a href="{{ route('user.follow', ['followed_id' => $user->id]) }}" class='btn btn-primary'>Follow</a>
-                                </div>
-                                @endif
                             </div>
                         </div>
 
